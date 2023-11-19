f677b9b81932d91774533cb5ddbb93124210c891
Wasja Zejkan <vasazejkan5@gmail.com>
2023-11-19 02:06:38 +0200

    Configured spring security: added classes: SecurityConfig, UserDatailServiceImpl

--- src/main/java/mate/academy/carsharing/repository/UserRepository.java
+++ src/main/java/mate/academy/carsharing/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package mate.academy.carsharing.repository;
 
+import java.util.Optional;
 import mate.academy.carsharing.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
