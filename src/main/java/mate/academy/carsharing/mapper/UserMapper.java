de9dd6d80eed27c1bd8850a5dfd71903c4c45ecd
Wasja Zejkan <vasazejkan5@gmail.com>
2023-11-25 23:22:41 +0200

    Added UserService with its implementation, UserController, correct AuthenticationController endpoints

--- src/main/java/mate/academy/carsharing/mapper/UserMapper.java
+++ src/main/java/mate/academy/carsharing/mapper/UserMapper.java
@@ -3,12 +3,16 @@ package mate.academy.carsharing.mapper;
 import mate.academy.carsharing.config.MapperConfig;
 import mate.academy.carsharing.dto.user.UserRegistrationRequestDto;
 import mate.academy.carsharing.dto.user.UserRegistrationResponseDto;
+import mate.academy.carsharing.dto.user.UserRequestDto;
 import mate.academy.carsharing.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
     User toModel(UserRegistrationRequestDto requestDto);
 
     UserRegistrationResponseDto toDto(User user);
+
+    User updateUserFromDto(UserRequestDto userRequestDto, @MappingTarget User user);
 }
